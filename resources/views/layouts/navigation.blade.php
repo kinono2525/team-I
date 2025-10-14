919cafb1105bd4daf60d2af73504196cd27af25f
tobakuro <tobakurow57@gmail.com>
2025-10-15 00:12:01 +0900

    テスト結果入力画面へのリンクを追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,9 @@
                     <x-nav-link :href="route('attendance.create')" :active="request()->routeIs('attendance.create')">
                         {{ __('出席登録') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('students.search')" :active="request()->routeIs('students.search')">
+                        {{ __('テスト結果入力') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -75,6 +78,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('attendance.create')" :active="request()->routeIs('attendance.create')">
                 {{ __('出席登録') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('students.search')" :active="request()->routeIs('students.search')">
+                {{ __('テスト結果入力') }}
             </x-responsive-nav-link>
         </div>
 
