89356a5d43ff63c0ff1758c8d69b1d9b0b11f471
tobakuro <tobakurow57@gmail.com>
2025-10-14 23:45:43 +0900

    出席登録画面へのルート追加

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/attendance', function () {
+        return view('attendance.create');
+    })->name('attendance.create');
 });
 
 require __DIR__.'/auth.php';
