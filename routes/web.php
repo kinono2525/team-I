8c1311dfa20bdc4e6ea65e92bde9a2c5e4fec524
tobakuro <tobakurow57@gmail.com>
2025-10-18 14:42:58 +0900

    出席登録画面からのPOSTリクエスト処理をするため、POSTルートを追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\WrongQuestionController;
+use App\Http\Controllers\AttendanceController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,11 +19,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/attendance', function () {
-        return view('attendance.create');
-    })->name('attendance.create');
-
+    Route::get('/wrong-questions', [WrongQuestionController::class, 'index'])->name('wrong_questions.index');
+    Route::post('/wrong-questions', [WrongQuestionController::class, 'store'])->name('wrong_questions.store');
+    Route::get('/attendance', [AttendanceController::class, 'create'])->name('attendance.create');
+    Route::post('/attendance', [AttendanceController::class, 'store'])->name('attendance.store');
     Route::get('/students/search', [StudentController::class, 'search'])->name('students.search');
 
     Route::prefix('students/{student}')->group(function () {
