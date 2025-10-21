5d5e079f064ea3b0fa6b7d1514228b477397aca5
tobakuro <tobakurow57@gmail.com>
2025-10-21 22:29:01 +0900

    ダッシュボード画面の文字を日本語に変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ホーム') }}
                     </x-nav-link>
                     <x-nav-link :href="route('students.search')" :active="request()->routeIs('students.search')">
                         {{ __('生徒選択') }}
